538ca592f5bab152b85cf4ffaade8630a12bd4e6
Prajnamayee Sahu <163574746+prajnamayeesahu@users.noreply.github.com>
2026-04-20 12:03:19 +0530

    Add: longest-turbulent-subarray - submission-0

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int l = 0, r = 1, res = 1;
+        string prev = "";
+
+        while (r < arr.size()) {
+            if (arr[r - 1] > arr[r] && prev != ">") {
+                res = max(res, r - l + 1);
+                r++;
+                prev = ">";
+            } else if (arr[r - 1] < arr[r] && prev != "<") {
+                res = max(res, r - l + 1);
+                r++;
+                prev = "<";
+            } else {
+                r = (arr[r] == arr[r - 1]) ? r + 1 : r;
+                l = r - 1;
+                prev = "";
+            }
+        }
+
+        return res;
+    }
+};
+//(nums[i] - nums[i-1]) * (nums[i+1] - nums[i]) < 0 -> zigzag patter <>< or ><>
