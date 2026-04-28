8b6fb37bb17a77a4c86eabc83f60253079c04e94
Prajnamayee Sahu <163574746+prajnamayeesahu@users.noreply.github.com>
2026-04-28 08:07:48 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+         int res = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // check overflow before multiplying
+            if (res > INT_MAX / 10 || res < INT_MIN / 10)
+                return 0;
+
+            res = res * 10 + digit;
+        }
+
+        return res;
+        
+    }
+};
