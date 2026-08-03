adc9afe0433772c7630dd2189b8e2989b3c4cfd0
Prajnamayee Sahu <163574746+prajnamayeesahu@users.noreply.github.com>
2026-08-03 10:17:00 +0530

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        vector<int> nodes;
+        for (ListNode* lst : lists) {
+            while (lst) {
+                nodes.push_back(lst->val);
+                lst = lst->next;
+            }
+        }
+        sort(nodes.begin(), nodes.end());
+
+        ListNode* res = new ListNode(0);
+        ListNode* cur = res;
+        for (int node : nodes) {
+            cur->next = new ListNode(node);
+            cur = cur->next;
+        }
+        return res->next;
+    }
+};
