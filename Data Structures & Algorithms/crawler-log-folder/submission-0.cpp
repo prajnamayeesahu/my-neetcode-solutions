9fe350bb3dc85a114fe8f8c7d5f53c826322aa77
Prajnamayee Sahu <163574746+prajnamayeesahu@users.noreply.github.com>
2026-09-08 15:05:58 +0530

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int res = 0;
+        for (auto& log : logs) {
+            if (log == "./") {
+                continue;
+            }
+            if (log == "../") {
+                res = max(0, res - 1);
+            } else {
+                res++;
+            }
+        }
+        return res;
+    }
+};
